e786cf8432a33dcc40374df17e936a4a020fd8d6
Mihaly Bogdan-Matei <bogdan.mihaly@student.upt.ro>
2023-12-16 21:26:30 +0200

    Preparing for 2024 Season

--- Python/ExtractData.py
+++ Python/ExtractData.py
@@ -483,6 +483,242 @@ def GetResults2023(index, name):
         for i in range(1, 21):
             db.reference("/Circuits/2023/" + name + "/" + "SprintQualifying" + str(i)).set("")
 
+def GetResults2024(index, name):
+    #Result
+    url = 'https://www.formula1.com/en/results.html/2024/races/'+index+'/'+name+'/race-result.html'
+    response = requests.get(url)
+    posDQ = 20
+    if response.status_code != 200:
+        print("Nu exista")
+        exit(0)
+    else:
+        print(name + "Race")
+        html_content = response.text
+
+        #Parsing the HTML Content
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        #The tabel will be found by class and ID
+        tabel = soup.find('table', {'class': 'resultsarchive-table'})
+        Text = "RACE"
+
+        desired_colums = [1, 2]
+        poz = -1
+        exist = 0
+        #Verify if the race has results
+        if tabel != None:
+            Valid = soup.find_all('h1', class_='ResultsArchiveTitle')
+            print(str(Valid).find(Text))
+            print(Text in str(Valid))
+            if(Text in str(Valid)):
+                #Go through all rows and columns from table to get data
+                for row in tabel.find_all('tr'):
+                    cells = row.find_all('td')
+                    poz = poz + 1
+                    exist = 1
+                    if len(cells) >= max(desired_colums) + 1:
+                        desired_data = [cells[columns].text for columns in desired_colums]
+                        if desired_data[0]=='DQ' or desired_data[0] == 'NC':
+                            desired_data[0] = str(posDQ)
+                            posDQ = posDQ - 1
+                            poz = poz -1
+                        #print(desired_data)
+                        ref = db.reference("/")
+                        db.reference("/Circuits/2024/"+name+"/"+"Place"+desired_data[0]).set(desired_data[1])
+                        PositionSeason[int(desired_data[1])] += int(desired_data[0])
+                        AppearanceSeason[int(desired_data[1])] += 1
+                        db.reference("/Circuits/2024/" + name + "/FutureRace").set("0")
+                        ref.get()
+        if exist == 0:
+            for i in range(1, 21):
+                db.reference("/Circuits/2024/" + name + "/" + "Place" + str(i)).set("")
+                db.reference("/Circuits/2024/" + name + "/FutureRace").set("1")
+
+    #Fastest Lap
+    url = 'https://www.formula1.com/en/results.html/2024/races/' + index + '/' + name + '/fastest-laps.html'
+    response = requests.get(url)
+    posDQ = 20
+    if response.status_code != 200:
+        print("Nu exista")
+        exit(0)
+    else:
+        print(name + "FastestLap")
+        html_content = response.text
+
+        # Parsing the HTML Content
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        # The tabel will be found by class and ID
+        tabel = soup.find('table', {'class': 'resultsarchive-table'})
+        Text = "FASTEST"
+
+        desired_colums = [1, 2]
+        poz = -1
+        exist = 0
+        # Verify if the race has results
+        if tabel != None:
+            Valid = soup.find_all('h1', class_='ResultsArchiveTitle')
+            print(str(Valid).find(Text))
+            print(Text in str(Valid))
+            if (Text in str(Valid)):
+                # Go through all rows and columns from table to get data
+                for row in tabel.find_all('tr'):
+                    cells = row.find_all('td')
+                    poz = poz + 1
+                    exist = 1
+                    if len(cells) >= max(desired_colums) + 1:
+                        desired_data = [cells[columns].text for columns in desired_colums]
+                        ref = db.reference("/")
+                        db.reference("/Circuits/2024/" + name + "/" + "FastestLap").set(desired_data[1])
+                        ref.get()
+                        break
+        if exist == 0:
+            for i in range(1, 21):
+                db.reference("/Circuits/2024/" + name + "/" + "FastestLap").set("")
+
+    #Qualifying
+    url = 'https://www.formula1.com/en/results.html/2024/races/' + index + '/' + name + '/qualifying.html'
+    response = requests.get(url)
+    posDQ = 20
+    if response.status_code != 200:
+        print("Nu exista")
+        exit(0)
+    else:
+        print(name + "Qualifying")
+        html_content = response.text
+
+        # Parsing the HTML Content
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        # The tabel will be found by class and ID
+        tabel = soup.find('table', {'class': 'resultsarchive-table'})
+        Text = "QUALIFYING"
+
+        desired_colums = [1, 2]
+        poz = -1
+        exist = 0
+        # Verify if the race has results
+        if tabel != None:
+            # Go through all rows and columns from table to get data
+            Valid = soup.find_all('h1', class_='ResultsArchiveTitle')
+            print(str(Valid).find(Text))
+            print(Text in str(Valid))
+            if(Text in str(Valid)):
+                for row in tabel.find_all('tr'):
+                    cells = row.find_all('td')
+                    poz = poz + 1
+                    exist = 1
+                    if len(cells) >= max(desired_colums) + 1:
+                        desired_data = [cells[columns].text for columns in desired_colums]
+                        if desired_data[0] == 'DQ' or desired_data[0] == 'NC':
+                            desired_data[0] = str(posDQ)
+                            posDQ = posDQ - 1
+                            poz = poz - 1
+                        # print(desired_data)
+                        ref = db.reference("/")
+                        db.reference("/Circuits/2024/" + name + "/" + "Qualifying" + desired_data[0]).set(desired_data[1])
+                        ref.get()
+        if exist == 0:
+            for i in range(1, 21):
+                db.reference("/Circuits/2024/" + name + "/" + "Qualifying" + str(i)).set("")
+
+
+    #Sprint
+    url = 'https://www.formula1.com/en/results.html/2024/races/' + index + '/' + name + '/sprint-results.html'
+    response = requests.get(url)
+    if name == "china" or name == "miami" or name == "austria" or name == "united_States" or name == "brazil" or name == "qatar" :
+        html_content = response.text
+        print(name + "Sprint")
+        posDQ = 20;
+
+        # Parsing the HTML Content
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        # The tabel will be found by class and ID
+        tabel = soup.find('table', {'class': 'resultsarchive-table'})
+        db.reference("/Circuits/2024/" + name + "/" + "SprintRace").set(str("1"))
+        desired_colums = [1, 2]
+        poz = -1
+        exist = 0
+        Text = "SPRINT"
+        # Verify if the race has results
+        if tabel != None:
+            # Go through all rows and columns from table to get data
+            Valid = soup.find_all('h1', class_='ResultsArchiveTitle')
+            print(str(Valid).find(Text))
+            print(Text in str(Valid))
+            if(Text in str(Valid)):
+                for row in tabel.find_all('tr'):
+                    cells = row.find_all('td')
+                    poz = poz + 1
+                    if len(cells) >= max(desired_colums) + 1:
+                        desired_data = [cells[columns].text for columns in desired_colums]
+                        exist = 1
+                        if desired_data[0] == 'DQ' or desired_data[0] == 'NC':
+                            desired_data[0] = str(posDQ)
+                            posDQ = posDQ - 1
+                            poz = poz - 1
+                        #print(desired_data)
+                        ref = db.reference("/")
+                        db.reference("/Circuits/2024/" + name + "/" + "Sprint" + desired_data[0]).set(desired_data[1])
+                        PositionSeasonSprint[int(desired_data[1])] += int(desired_data[0])
+                        AppearanceSeasonSprint[int(desired_data[1])] += 1
+                        ref.get()
+        if exist == 0:
+            for i in range(1, 21):
+                db.reference("/Circuits/2024/" + name + "/" + "Sprint" + str(i)).set("")
+    else:
+        for i in range (1,21):
+            db.reference("/Circuits/2024/" + name + "/" + "Sprint" + str(i)).set("")
+        db.reference("/Circuits/2024/" + name + "/" + "SprintRace").set(str("0"))
+
+    #Sprint Shootout
+    url = 'https://www.formula1.com/en/results.html/2024/races/' + index + '/' + name + '/sprint-shootout.html'
+    response = requests.get(url)
+    if name == "china" or name == "miami" or name == "austria" or name == "united_States" or name == "brazil" or name == "qatar" :
+        html_content = response.text
+        print(name + "Sprint Shootout")
+        posDQ = 20
+
+        # Parsing the HTML Content
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        # The tabel will be found by class and ID
+        tabel = soup.find('table', {'class': 'resultsarchive-table'})
+        db.reference("/Circuits/2024/" + name + "/" + "SprintRace").set(str("1"))
+        desired_colums = [1, 2]
+        poz = -1
+        exist = 0
+
+        Text = "SPRINT SHOOTOUT"
+        # Verify if the race has results
+        if tabel != None:
+            # Go through all rows and columns from table to get data
+            Valid = soup.find_all('h1', class_='ResultsArchiveTitle')
+            print(str(Valid).find(Text))
+            print(Text in str(Valid))
+            if (Text in str(Valid)):
+                for row in tabel.find_all('tr'):
+                    cells = row.find_all('td')
+                    poz = poz + 1
+                    exist = 1
+                    if len(cells) >= max(desired_colums) + 1:
+                        desired_data = [cells[columns].text for columns in desired_colums]
+                        if desired_data[0] == 'DQ' or desired_data[0] == 'NC':
+                            desired_data[0] = str(posDQ)
+                            posDQ = posDQ - 1
+                            poz = poz - 1
+                        # print(desired_data)
+                        ref = db.reference("/")
+                        db.reference("/Circuits/2024/" + name + "/" + "SprintQualifying" + desired_data[0]).set(desired_data[1])
+                        ref.get()
+            if exist == 0:
+                for i in range(1, 21):
+                    db.reference("/Circuits/2024/" + name + "/" + "SprintQualifying" + str(i)).set("")
+    else:
+        for i in range(1, 21):
+            db.reference("/Circuits/2024/" + name + "/" + "SprintQualifying" + str(i)).set("")
+
 def GetCircuitData2020(name):
     #Get data about the circuits
     if "saudi" in name:
@@ -775,6 +1011,84 @@ def GetCircuitData2023(name):
 
     ref.get()
 
+def GetCircuitData2024(name):
+    #Get data about the circuits
+    if "saudi" in name:
+        url = 'https://www.formula1.com/en/racing/2024/Saudi_Arabia/Circuit.html'
+    elif "great" in name:
+        url = 'https://www.formula1.com/en/racing/2024/Great_Britain/Circuit.html'
+    elif "united" in name:
+        url = 'https://www.formula1.com/en/racing/2024/United_States/Circuit.html'
+    elif "las" in name:
+        url = 'https://www.formula1.com/en/racing/2024/Las_Vegas/Circuit.html'
+    elif "abu" in name:
+        url = 'https://www.formula1.com/en/racing/2024/United_Arab_Emirates/Circuit.html'
+    elif "emilia" in name:
+        url = 'https://www.formula1.com/en/racing/2024/EmiliaRomagna/Circuit.html'
+    else:
+        url = 'https://www.formula1.com/en/racing/2024/' + name.capitalize() + '/Circuit.html'
+    print(url)
+
+    response = requests.get(url)
+    html_content = response.text
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    AllData = soup.find_all('p', class_='f1-bold--stat')
+    #print(AllData)
+
+    FirstGP = AllData[0].get_text(strip=True)
+    NoLaps = AllData[1].get_text(strip=True)
+    CircuitLength = AllData[2].get_text(strip=True)
+    RaceDistance = AllData[3].get_text(strip=True)
+    LapRecord = AllData[4].get_text(strip=True)
+
+    #print(FirstGP)
+    #print(CircuitLength)
+    #print(NoLaps)
+    #print(RaceDistance)
+    #print(LapRecord)
+
+    ref = db.reference("/")
+    db.reference("/Circuits/2024/" + name + "/CircuitName").set(name)
+    db.reference("/Circuits/2024/" + name + "/FirstGP").set(FirstGP)
+    db.reference("/Circuits/2024/" + name + "/CircuitLength").set(CircuitLength)
+    db.reference("/Circuits/2024/" + name + "/NoLaps").set(NoLaps)
+    db.reference("/Circuits/2024/" + name + "/RaceDistance").set(RaceDistance)
+    db.reference("/Circuits/2024/" + name + "/LapRecord").set(LapRecord)
+
+    #Take DateF
+    if "saudi" in name:
+        url = 'https://www.formula1.com/en/racing/2024/Saudi_Arabia.html'
+    elif "great" in name:
+        url = 'https://www.formula1.com/en/racing/2024/Great_Britain.html'
+    elif "united" in name:
+        url = 'https://www.formula1.com/en/racing/2024/United_States.html'
+    elif "las" in name:
+        url = 'https://www.formula1.com/en/racing/2024/Las_Vegas.html'
+    elif "abu" in name:
+        url = 'https://www.formula1.com/en/racing/2024/United_Arab_Emirates.html'
+    elif "emilia" in name:
+        url = 'https://www.formula1.com/en/racing/2024/EmiliaRomagna.html'
+    else:
+        url = 'https://www.formula1.com/en/racing/2024/' + name.capitalize()+'.html'
+
+    url = 'https://www.autosport.com/f1/schedule/2024/'
+    response = requests.get(url)
+    html_content = response.text
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    Date = soup.find_all('span', class_='ms-schedule-table-date__value text-md font-semi uppercase')
+    DateGood = Date[2*pos].get_text(strip=True)
+
+    db.reference("/Circuits/2024/" + name + "/Date").set(DateGood)
+    print(name)
+    print(pos)
+    print(DateGood)
+
+    ref.get()
+
 def DriversDetails():
     #Get actual drivers names and points
     url = 'https://www.formula1.com/en/drivers.html'
@@ -934,6 +1248,8 @@ circuits2022 = ["bahrain", "saudi-arabia", "australia", "miami", "spain", "monac
 indexes2022 = ["1124", "1125", "1108", "1110", "1111", "1112", "1126", "1113", "1114", "1115", "1116", "1117", "1118", "1119", "1120", "1133", "1134", "1135", "1136", "1137", "1138"]
 circuits2023 = ["bahrain", "saudi-arabia", "australia", "azerbaijan", "miami", "monaco", "spain", "canada", "austria", "great_britain", "hungary", "belgium", "netherlands", "italy", "singapore", "japan", "qatar", "united_States", "mexico", "brazil", "las_vegas", "abu_dhabi"]
 indexes2023 = ["1141", "1142", "1143", "1207", "1208", "1210", "1211", "1212", "1213", "1214", "1215", "1216", "1217", "1218", "1219", "1220", "1221", "1222", "1223", "1224", "1225", "1226"]
+circuits2024 = ["bahrain", "saudi-arabia", "australia", "japan", "china", "miami", "emiliaRomagna", "canada", "spain", "austria", "great_britain", "hungary", "belgium", "netherlands", "italy", "azerbaijan", "singapore", "united_States", "mexico", "brazil", "las_vegas", "qatar", "abu_dhabi"]
+indexes2024 = ["1229", "1230", "1231", "1232", "1233", "1234", "1235", "1236", "1237", "1238", "1239", "1240", "1241", "1242", "1243", "1244", "1245", "1246", "1247", "1248", "1249", "1250", "1251", "1252"]
 
 #Url for teams details
 urlteams = "https://www.formula1.com/en/teams/"
@@ -949,8 +1265,10 @@ firebase_admin.initialize_app(cred, {"databaseURL": "https://licenta-ed6d3-defau
 #    GetCircuitData2021(circuits2021[pos])
 #for pos, element in enumerate(circuits2022):
 #    GetCircuitData2022(circuits2022[pos])
-for pos, element in enumerate(circuits2023):
-    GetCircuitData2023(circuits2023[pos])
+#for pos, element in enumerate(circuits2023):
+#    GetCircuitData2023(circuits2023[pos])
+#for pos, element in enumerate(circuits2024):
+#    GetCircuitData2024(circuits2024[pos])
 
 #Go through all results
 #for pos, element in enumerate(indexes2020):
@@ -959,8 +1277,10 @@ for pos, element in enumerate(circuits2023):
 #    GetResults2021(indexes2021[pos], circuits2021[pos])
 #for pos, element in enumerate(indexes2022):
 #    GetResults2022(indexes2022[pos], circuits2022[pos])
-for pos, element in enumerate(indexes2023):
-    GetResults2023(indexes2023[pos], circuits2023[pos])
+#for pos, element in enumerate(indexes2023):
+#    GetResults2023(indexes2023[pos], circuits2023[pos])
+for pos, element in enumerate(indexes2024):
+    GetResults2024(indexes2024[pos], circuits2024[pos])
 
 
 #Everytime run this 2 with GetResults2023
